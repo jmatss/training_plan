8a83e402dad4e7220d6e87925d60fbc5979d2132
jmatss <jmatss@kth.se>
2020-06-27 16:33:09 +0200

    Add `sellIn` variable for clarification

--- GildedRose_kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose_kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,9 +8,10 @@ class GildedRoseTest {
     @Test
     public void TestSulfurasAlwaysHasQuality80() {
         // ARRANGE
+        int sellIn = 0;
         int expectedQuality = 80;
 
-        Item actualItem = new Item(GildedRose.SULFURAS, 0, expectedQuality);
+        Item actualItem = new Item(GildedRose.SULFURAS, sellIn, expectedQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -24,10 +25,11 @@ class GildedRoseTest {
     @Test
     public void TestAgedBrieIncresesQualityByOneWhenSellInIsPositive() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 5;
         int expectedQuality = initQuality + 1;
 
-        Item actualItem = new Item(GildedRose.AGED_BRIE, 1, initQuality);
+        Item actualItem = new Item(GildedRose.AGED_BRIE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -41,10 +43,11 @@ class GildedRoseTest {
     @Test
     public void TestAgedBrieIncresesQualityByTwoWhenSellInIsNegativeOrZero() {
         // ARRANGE
+        int sellIn = 0;
         int initQuality = 5;
         int expectedQuality = initQuality + 2;
 
-        Item actualItem = new Item(GildedRose.AGED_BRIE, 0, initQuality);
+        Item actualItem = new Item(GildedRose.AGED_BRIE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -58,10 +61,11 @@ class GildedRoseTest {
     @Test
     public void TestAgedBrieNeverIncresesQualityOver50() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 50;
         int expectedQuality = initQuality;
 
-        Item actualItem = new Item(GildedRose.AGED_BRIE, 1, initQuality);
+        Item actualItem = new Item(GildedRose.AGED_BRIE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -75,10 +79,11 @@ class GildedRoseTest {
     @Test
     public void TestBackstageIncresesQualityByOneWhenSellInIsOver10() {
         // ARRANGE
+        int sellIn = 11;
         int initQuality = 0;
         int expectedQuality = initQuality + 1;
 
-        Item actualItem = new Item(GildedRose.BACKSTAGE, 11, initQuality);
+        Item actualItem = new Item(GildedRose.BACKSTAGE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -92,10 +97,11 @@ class GildedRoseTest {
     @Test
     public void TestBackstageIncresesQualityByTwoWhenSellInIsBetween6And10() {
         // ARRANGE
+        int sellIn = 10;
         int initQuality = 0;
         int expectedQuality = initQuality + 2;
 
-        Item actualItem = new Item(GildedRose.BACKSTAGE, 10, initQuality);
+        Item actualItem = new Item(GildedRose.BACKSTAGE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -109,10 +115,11 @@ class GildedRoseTest {
     @Test
     public void TestBackstageIncresesQualityByThreeWhenSellInIsBetween1And5() {
         // ARRANGE
+        int sellIn = 5;
         int initQuality = 0;
         int expectedQuality = initQuality + 3;
 
-        Item actualItem = new Item(GildedRose.BACKSTAGE, 5, initQuality);
+        Item actualItem = new Item(GildedRose.BACKSTAGE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -126,10 +133,11 @@ class GildedRoseTest {
     @Test
     public void TestBackstageDropsQualityToZeroWhenSellInIsZero() {
         // ARRANGE
+        int sellIn = 0;
         int initQuality = 5;
         int expectedQuality = 0;
 
-        Item actualItem = new Item(GildedRose.BACKSTAGE, 0, initQuality);
+        Item actualItem = new Item(GildedRose.BACKSTAGE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -143,10 +151,11 @@ class GildedRoseTest {
     @Test
     public void TestBackstageNeverIncresesQualityOver50() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 50;
         int expectedQuality = initQuality;
 
-        Item actualItem = new Item(GildedRose.BACKSTAGE, 1, initQuality);
+        Item actualItem = new Item(GildedRose.BACKSTAGE, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -160,10 +169,11 @@ class GildedRoseTest {
     @Test
     public void TestConjuredDecreasesQualityByTwoWhenSellInIsPositive() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 5;
         int expectedQuality = initQuality - 2;
 
-        Item actualItem = new Item(GildedRose.CONJURED, 1, initQuality);
+        Item actualItem = new Item(GildedRose.CONJURED, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -177,10 +187,11 @@ class GildedRoseTest {
     @Test
     public void TestConjuredDecreasesQualityByFourWhenSellInIsNegativeOrZero() {
         // ARRANGE
+        int sellIn = 0;
         int initQuality = 5;
         int expectedQuality = initQuality - 4;
 
-        Item actualItem = new Item(GildedRose.CONJURED, 0, initQuality);
+        Item actualItem = new Item(GildedRose.CONJURED, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -194,10 +205,11 @@ class GildedRoseTest {
     @Test
     public void TestConjuredNeverDecreasesQualityUnderZero() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 0;
         int expectedQuality = initQuality;
 
-        Item actualItem = new Item(GildedRose.CONJURED, 1, initQuality);
+        Item actualItem = new Item(GildedRose.CONJURED, sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -211,10 +223,11 @@ class GildedRoseTest {
     @Test
     public void TestNormalItemDecreasesQualityByOnwWhenSellInIsPositive() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 5;
         int expectedQuality = initQuality - 1;
 
-        Item actualItem = new Item("NORMAL ITEM", 1, initQuality);
+        Item actualItem = new Item("NORMAL ITEM", sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -228,10 +241,11 @@ class GildedRoseTest {
     @Test
     public void TestNormalItemDecreasesQualityByTwoWhenSellInIsNegativeOrZero() {
         // ARRANGE
+        int sellIn = 0;
         int initQuality = 5;
         int expectedQuality = initQuality - 2;
 
-        Item actualItem = new Item("NORMAL ITEM", 0, initQuality);
+        Item actualItem = new Item("NORMAL ITEM", sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
@@ -245,10 +259,11 @@ class GildedRoseTest {
     @Test
     public void TestNormalItemNeverDecreasesQualityUnderZero() {
         // ARRANGE
+        int sellIn = 1;
         int initQuality = 0;
         int expectedQuality = initQuality;
 
-        Item actualItem = new Item("NORMAL ITEM", 1, initQuality);
+        Item actualItem = new Item("NORMAL ITEM", sellIn, initQuality);
         Item[] items = { actualItem };
         GildedRose app = new GildedRose(items);
 
